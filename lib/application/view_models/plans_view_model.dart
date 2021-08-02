cc9fd2ceecdb928f1b6921b20d155a3524acd1b8
hayabusabusa <hayabusabusaSY@gmail.com>
2021-08-02 23:05:05 +0900

    feat: add plans view model

---
+++ lib/application/view_models/plans_view_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:shiori/application/models/models.dart';
+import 'package:shiori/shared/shared.dart';
+
+class PlansViewModel extends ChangeNotifier {
+
+  // MARK: Dependency
+
+  final PlansModelable _model;
+
+  PlansViewModel(this._model) {
+    _model.isLoading.listen((event) { 
+      _isLoading = event;
+      notifyListeners();
+    });
+    _model.plans.listen((event) {
+      _plans = event;
+      notifyListeners();
+    });
+
+    _model.fetch();
+  }
+
+  // MARK: Properties
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  List<Plan> _plans = [];
+  List<Plan> get plans => _plans;
+
+  // MARK: Public
+
+  @override
+  void dispose() {
+    _model.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/application/view_models/view_models.dart
@@ -0,0 +1,1 @@
+export 'plans_view_model.dart';
