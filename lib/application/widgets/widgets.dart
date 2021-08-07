db8ca2fec6f8584aa0b4c00e548df10eecd4dfc5
hayabusabusa <hayabusabusaSY@gmail.com>
2021-08-07 20:31:04 +0900

    ui: add platform indicator

--- lib/application/widgets/widgets.dart
+++ lib/application/widgets/widgets.dart
@@ -4,5 +4,6 @@ export 'plan_detail_header.dart';
 export 'plan_detail_timeline_end.dart';
 export 'plan_detail_timeline.dart';
 export 'plans_cell.dart';
+export 'platform_indicator.dart';
 export 'prices_cell.dart';
 export 'prices_total_cell.dart';
