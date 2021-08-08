e5ca949c19b7aeaf28904a8a0483c0ab0cab30f3
hayabusabusa <hayabusabusaSY@gmail.com>
2021-08-09 03:39:22 +0900

    ui: add logo images

--- lib/application/screens/splash_screen.dart
+++ lib/application/screens/splash_screen.dart
@@ -6,9 +6,29 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Center(
-        child: Image.asset(''),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // UI: Logo
+            Image.asset(
+              'assets/logo.png',
+              width: 56,
+              height: 56,
+            ),
+            const SizedBox(height: 12,),
+            // UI: Text
+            Text(
+              'SHIORI',
+              style: Theme.of(context).textTheme.overline?.merge(
+                TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                )
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
