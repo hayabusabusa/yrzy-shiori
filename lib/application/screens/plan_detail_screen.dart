0b90beba3e5eba617e077f4a31510b983bd928a6
hayabusabusa <hayabusabusaSY@gmail.com>
2021-08-17 23:42:00 +0900

    ui: fix plan detail screen layout

--- lib/application/screens/plan_detail_screen.dart
+++ lib/application/screens/plan_detail_screen.dart
@@ -19,74 +19,68 @@ class PlanDetailScreen extends StatelessWidget {
 
   PlanDetailScreen._({ Key? key }) : super(key: key);
 
-  Widget _buildTimelineSliverList(BuildContext context) {
-    final isLoading = context.select((PlanDetailViewModel viewModel) => viewModel.isLoading);
-    //final timelines = context.select((PlanDetailViewModel viewModel) => viewModel.timelines);
-    return isLoading 
-      ? SliverToBoxAdapter(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0),
-              child: CircularProgressIndicator(
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-          ),
-        )
-      : SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              // 現在地を仮に2とする.
-              final currentIndex = 2;
-              final isCurrent = index == currentIndex;
-              final isPassed = index < currentIndex;
-              final isNext = (index - 1) == currentIndex;
-              if (index == 0) {
-                // 開始地点のセル.
-                return PlanDetailTimeline.start(isCurrent: isCurrent, isPassed: isPassed,);
-              } else if (index == 4) {
-                // 帰宅地点のセル.
-                return PlanDetailTimelineEnd(isCurrent: isCurrent,);
-              } else {
-                // 通過途中のセル.
-                return PlanDetailTimeline(isCurrent: isCurrent, isPassed: isPassed, isNext: isNext,);
-              }
-            },
-            childCount: 5,
-          ),
-        );
-  }
-
   @override
   Widget build(BuildContext context) {
     final plan = context.select((PlanDetailViewModel viewModel) => viewModel.plan);
+    final isLoading = context.select((PlanDetailViewModel viewModel) => viewModel.isLoading);
+    //final timelines = context.select((PlanDetailViewModel viewModel) => viewModel.timelines);
     return Scaffold(
       appBar: AppBar(
         title: Text('予定詳細'),
       ),
-      body: Scrollbar(
-        child: CustomScrollView(
-          slivers: [
-            // UI: Header
-            SliverToBoxAdapter(
-              child: PlanDetailHeader(
-                title: plan.title,
-                destination: plan.destination,
-                dateString: '${plan.departureDate.formattedString('MM月dd日')} - ${plan.homeDate.formattedString('MM月dd日')}',
-                totalPrice: plan.totalPrice,
-                description: plan.description,
-                onTapPrices: () {
-                  Navigator.of(context).pushNamed(ApplicationRouter.prices);
-                },
-                onTapBrings: () {
-                  Navigator.of(context).pushNamed(ApplicationRouter.brings);
-                },
-              ),
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 400),
+        child: isLoading 
+          ? Center(
+            child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
             ),
-            // UI: Timelines
-            _buildTimelineSliverList(context),
-          ],
-        ),
+          )
+          : Scrollbar(
+            child: CustomScrollView(
+              slivers: [
+                // UI: Header
+                SliverToBoxAdapter(
+                  child: PlanDetailHeader(
+                    title: plan.title,
+                    destination: plan.destination,
+                    dateString: '${plan.departureDate.formattedString('MM月dd日')} - ${plan.homeDate.formattedString('MM月dd日')}',
+                    totalPrice: plan.totalPrice,
+                    description: plan.description,
+                    onTapPrices: () {
+                      Navigator.of(context).pushNamed(ApplicationRouter.prices);
+                    },
+                    onTapBrings: () {
+                      Navigator.of(context).pushNamed(ApplicationRouter.brings);
+                    },
+                  ),
+                ),
+                // UI: Timelines
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      // 現在地を仮に2とする.
+                      final currentIndex = 2;
+                      final isCurrent = index == currentIndex;
+                      final isPassed = index < currentIndex;
+                      final isNext = (index - 1) == currentIndex;
+                      if (index == 0) {
+                        // 開始地点のセル.
+                        return PlanDetailTimeline.start(isCurrent: isCurrent, isPassed: isPassed,);
+                      } else if (index == 4) {
+                        // 帰宅地点のセル.
+                        return PlanDetailTimelineEnd(isCurrent: isCurrent,);
+                      } else {
+                        // 通過途中のセル.
+                        return PlanDetailTimeline(isCurrent: isCurrent, isPassed: isPassed, isNext: isNext,);
+                      }
+                    },
+                    childCount: 5,
+                  ),
+                ),
+              ],
+            ),
+          ),
       ),
     );
   }
