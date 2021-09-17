e445e9b9414a6ff2bb776b9a92bbedda84b09aea
hayabusabusa <hayabusabusaSY@gmail.com>
2021-09-17 20:21:39 +0900

    feat: add brings model

--- lib/application/models/brings_model.dart
+++ lib/application/models/brings_model.dart
@@ -3,49 +3,47 @@ import 'package:rxdart/rxdart.dart';
 import 'package:shiori/domain/domain.dart';
 import 'package:shiori/shared/shared.dart';
 
-abstract class PricesModelable {
+abstract class BringsModelable {
   /// ロード中かどうかが流れてくる `Stream<bool>`.
   Stream<bool> get isLoading;
-  /// Firestore から取得してきた費用一覧 `Stream<List<Price>>`.
-  Stream<List<Price>> get prices;
+  /// Firestore から取得してきた荷物一覧 `Stream<List<Bring>>`.
+  Stream<List<Bring>> get brings;
   /// 発生したエラーを流す `Stream<Error>`.
   Stream<Error> get error;
-  /// 費用一覧を取得する.
+  /// 荷物一覧を取得する.
   void fetch();
   /// リソースの解放.
   void dispose();
 }
 
-class PricesModel extends PricesModelable {
+class BringsModel extends BringsModelable {
 
   // MARK: Dependency
 
   Plan _plan;
   FirestoreService _firestoreService;
 
-  PricesModel({
-    required FirestoreService firestoreService,
+  BringsModel({
     required Plan plan,
-  }): this._firestoreService = firestoreService,
-      this._plan = plan;
-
+    required FirestoreService firestoreService,
+  }): this._plan = plan, 
+      this._firestoreService = firestoreService;
+    
   // MARK: Properties
 
   final BehaviorSubject<bool> _isLoadingSubject = BehaviorSubject<bool>.seeded(true);
   Stream<bool> get isLoading => _isLoadingSubject.stream;
-  final BehaviorSubject<List<Price>> _pricesSubject = BehaviorSubject<List<Price>>.seeded([]);
-  Stream<List<Price>> get prices => _pricesSubject.stream;
+  final BehaviorSubject<List<Bring>> _bringsSubject = BehaviorSubject<List<Bring>>.seeded([]);
+  Stream<List<Bring>> get brings => _bringsSubject.stream;
   final PublishSubject<Error> _errorSubject = PublishSubject<Error>();
   Stream<Error> get error => _errorSubject.stream;
-
-  // MARK: Public
 
   void fetch() async {
     _isLoadingSubject.add(true);
     try {
-      final snapshot = await _firestoreService.getNestedDocuments(collection: Collection.Plans, path: _plan.id, subCollection: SubCollection.Prices);
-      final decoded = snapshot.docs.map((e) => Price.fromData(id: e.id, data: e.data())).toList();
-      _pricesSubject.add(decoded);
+      final snapshot = await _firestoreService.getNestedDocuments(collection: Collection.Plans, path: _plan.id, subCollection: SubCollection.Brings);
+      final decoded = snapshot.docs.map((e) => Bring.fromData(id: e.id, data: e.data())).toList();
+      _bringsSubject.add(decoded);
     } on Error catch(error) {
       _errorSubject.add(error);
     } catch(error) {
@@ -56,7 +54,7 @@ class PricesModel extends PricesModelable {
 
   void dispose() {
     _isLoadingSubject.close();
-    _pricesSubject.close();
+    _bringsSubject.close();
     _errorSubject.close();
   }
 }
--- lib/application/models/models.dart
+++ lib/application/models/models.dart
@@ -1,3 +1,4 @@
+export 'brings_model.dart';
 export 'plan_detail_model.dart';
 export 'plans_model.dart';
 export 'prices_model.dart';
